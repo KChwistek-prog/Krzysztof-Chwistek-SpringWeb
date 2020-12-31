84f98b8c8a283b1c4d2b7ff4e26a13df1192dbad
KChwistek <66901714+KChwistek-prog@users.noreply.github.com>
2020-12-31 19:53:26 +0100

    21.4

--- showtasks.sh
+++ showtasks.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-
 runRunCrud() {
   echo "Executing runcrud script"
   sh runcrud.sh
 }
+
 openBrowser() {
   echo "Starting Edge with getTasks page"
   open -a /Applications/Microsoft\ Edge.app/Contents/MacOS/Microsoft\ Edge http://localhost:8080/crud/v1/task/getTasks

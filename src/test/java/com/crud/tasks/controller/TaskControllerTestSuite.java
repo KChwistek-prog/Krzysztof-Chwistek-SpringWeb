2d005364bf73791fc956e010b9947550f3611d8b
KChwistek <66901714+KChwistek-prog@users.noreply.github.com>
2021-03-10 16:27:38 +0100

    29.3

--- src/test/java/com/crud/tasks/controller/TaskControllerTestSuite.java
+++ src/test/java/com/crud/tasks/controller/TaskControllerTestSuite.java
@@ -64,8 +64,11 @@ public class TaskControllerTestSuite {
         Gson gson = new Gson();
         TaskDto taskDto = new TaskDto(1L, "Updated test title", "Updated test content");
         Task updatedTask = new Task(1L, "Updated test title", "Updated test content");
+        when(taskMapper.mapToTaskDto(any())).thenReturn(taskDto);
+        when(taskMapper.mapToTask(any())).thenReturn(updatedTask);
         when(dbService.updateTask(any())).thenReturn(updatedTask);
         String jsonTask = gson.toJson(updatedTask);
+
         //When & Then
         this.mockMvc
                 .perform(MockMvcRequestBuilders
@@ -73,7 +76,11 @@ public class TaskControllerTestSuite {
                         .contentType(MediaType.APPLICATION_JSON)
                         .characterEncoding("UTF-8")
                         .content(jsonTask))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title", Matchers.is("Updated test title")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content", Matchers.is("Updated test content")));
+
     }
 
     @Test
@@ -106,17 +113,20 @@ public class TaskControllerTestSuite {
     @Test
     void testGetTasks() throws Exception {
         //Given
-        Task firstTask = new Task(1L, "Test title", "Test content");
-        Task secondTask = new Task(2L, "Test title", "Test content");
-        List<Task> taskList = new ArrayList<>();
+        TaskDto firstTask = new TaskDto(1L, "Test title", "Test content");
+        TaskDto secondTask = new TaskDto(2L, "Test title", "Test content");
+        List<TaskDto> taskList = new ArrayList<>();
+        List<Task> taskList2 = new ArrayList<>();
         taskList.add(firstTask);
         taskList.add(secondTask);
-        when(dbService.getAllTasks()).thenReturn(taskList);
+        when(taskMapper.mapToTaskDtoList(any())).thenReturn(taskList);
+        when(dbService.getAllTasks()).thenReturn(taskList2);
         //When & Then
         this.mockMvc
                 .perform(MockMvcRequestBuilders
                         .get("/v1/task/getTasks"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)));
     }
 
 }
